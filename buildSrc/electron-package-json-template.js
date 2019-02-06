17a950f825d3251d12fa74645634934ffb22fd5d
nig <nig@tutao.de>
2019-02-06 11:50:26 +0100

    bump electron to v4.0.4

--- buildSrc/electron-package-json-template.js
+++ buildSrc/electron-package-json-template.js
@@ -32,7 +32,7 @@ module.exports = function (nameSuffix, version, targetUrl, iconPath, sign) {
 			}
 		},
 		"dependencies": {
-			"electron-updater": "4.0.6",
+			"electron-updater": "4.0.7",
 			"electron-localshortcut": "3.1.0",
 			"fs-extra": "7.0.1",
 			"bluebird": "3.5.2",
@@ -41,7 +41,7 @@ module.exports = function (nameSuffix, version, targetUrl, iconPath, sign) {
 		},
 		"build": {
 			"afterAllArtifactBuild": "./buildSrc/afterAllArtifactBuild.js",
-			"electronVersion": "4.0.2",
+			"electronVersion": "4.0.4",
 			"icon": iconPath,
 			"appId": "de.tutao.tutanota" + nameSuffix,
 			"productName": nameSuffix.length > 0
@@ -86,7 +86,7 @@ module.exports = function (nameSuffix, version, targetUrl, iconPath, sign) {
 				]
 			},
 			"nsis": {
-				"oneClick": false,"perMachine": false,
+				"oneClick": false, "perMachine": false,
 				"createStartMenuShortcut": true,
 				"allowElevation": true,
 				"allowToChangeInstallationDirectory": true
